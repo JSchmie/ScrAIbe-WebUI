989ba150c895faa914cf75c1d8871df4c7c5084b
Schmieder, Jacob <jacob.schmieder@dbfz.de>
2024-03-27 14:23:23 +0000

    reformated sync interface

---
+++ scraibe_webui/sync/__init__.py
@@ -0,0 +1,6 @@
+from .app import *
+from .interactions import *
+from .interface import *
+from .multi import *
+from .loader import *
+from .global_var import *
---
+++ scraibe_webui/sync/interactions.py
@@ -0,0 +1,77 @@
+"""
+This file contains ervery function that will be called when the user interacts with the 
+UI like pressing a button or uploading a file.
+"""
+
+import gradio as gr 
+import scraibe_webui.sync.global_var as gv
+from .multi import start_model_worker
+
+def run_scraibe(task,
+                num_speakers,
+                translate,
+                language,
+                audio1,
+                audio2,
+                video1,
+                video2,
+                file_in):
+    
+    if gv.MODELS_PROCESS.is_alive():
+        #progress(0.0, desc='Loading model...')
+        gv.MODELS_PROCESS = start_model_worker(gv.MODELS_PARAMS,
+                                      gv.REQUEST_QUEUE,
+                                      gv.LAST_ACTIVE_TIME,
+                                      gv.RESPONSE_QUEUE,
+                                      gv.LOADED_EVENT,
+                                      gv.RUNNING_EVENT)
+    
+    # progress(0.1, desc='Starting task...')
+    source = audio1 or audio2 or video1 or video2 or file_in
+    
+    if isinstance(source, list):
+        source = [s.name for s in source]
+        if len(source) == 1:
+            source = source[0]
+    
+    config = dict(source = source,
+                  task = task,
+                  num_speakers = num_speakers,
+                  translate = translate,
+                  language = language)
+    
+    gv.REQUEST_QUEUE.put(config)
+    
+    if task == 'Auto Transcribe':
+        
+        out_str , out_json = gv.RESPONSE_QUEUE.get()
+        
+        if isinstance(source, str):
+            return (gr.update(value = out_str, visible = True),
+                    gr.update(value = out_json, visible = True),
+                    gr.update(visible = True),
+                    gr.update(visible = True))      
+        else:
+            return (gr.update(value = out_str, visible = True),
+                    gr.update(value = out_json, visible = True),
+                    gr.update(visible = False),
+                    gr.update(visible = False))  
+        
+    elif task == 'Transcribe':
+        
+        out = gv.RESPONSE_QUEUE.get()
+        
+        return (gr.update(value = out, visible = True),
+                gr.update(value = None, visible = False),
+                gr.update(visible = False),
+                gr.update(visible = False))
+        
+    elif task == 'Diarisation':
+        
+        out = gv.RESPONSE_QUEUE.get()
+        
+        return (gr.update(value = None, visible = False),
+                gr.update(value = out, visible = True),
+                gr.update(visible = False),
+                gr.update(visible = False))
+    

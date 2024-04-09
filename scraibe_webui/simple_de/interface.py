6cc862db8d6a0f27a93cc1d7c4566aff14937446
Schmieder, Jacob <jacob.schmieder@dbfz.de>
2024-04-09 12:11:19 +0000

    removed .change function where it was not necessary

--- scraibe_webui/simple_de/interface.py
+++ scraibe_webui/simple_de/interface.py
@@ -113,13 +113,6 @@ def gradio_Interface(layout : dict) -> gr.Blocks:
             task.change(fn=select_task, inputs=[task],
                         outputs=[num_speakers, translate, language])
             
-            translate.change(fn= lambda x : gr.update(value = x),
-                                inputs=[translate], outputs=[translate])
-            num_speakers.change(fn= lambda x : gr.update(value = x),
-                                inputs=[num_speakers], outputs=[num_speakers])
-            language.change(fn= lambda x : gr.update(value = x), 
-                            inputs=[language], outputs=[language])
-            
             submit.click(fn = run_scraibe, 
                             inputs= [task, num_speakers, translate, language, audio, video, file_in],
                             outputs=[out_txt, out_json, annotation, annotate], concurrency_limit = None)

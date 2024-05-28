a74b33649bd58cb4491b52af1d737eb51ed7489d
Schmieder, Jacob <jacob.schmieder@dbfz.de>
2024-05-28 15:28:34 +0000

    updated App to be a class

--- scraibe_webui/app.py
+++ scraibe_webui/app.py
@@ -2,59 +2,33 @@
 from .utils.appconfigloader import AppConfigLoader
 from .ui import gradio_Interface
 
+class App:
+    def __init__(self, config: str = None, **kwargs):
+        """
+        Initializes the App class.
 
-def app(config : str = None, **kwargs):
-    """
-    Launches the Gradio interface for audio transcription.
+        Args:
+            config (str): Path to the YAML configuration file. Default settings are used 
+                          if not provided.
+            **kwargs: Keyword arguments corresponding to the configuration sections. Each 
+                      argument should be a dictionary reflecting the structure of its 
+                      respective section in `config.yaml`.
+        """
+        self.config = AppConfigLoader.load_config(config, **kwargs)
 
-    Initializes the Gradio web interface with settings from a YAML configuration file
-    and/or keyword arguments. The function manages AI models, handling their loading 
-    into RAM and unloading after a session or specified timeout.
+    def launch(self):
+        """
+        Launches the Gradio interface for audio transcription.
 
-    The `kwargs` are used to override or supplement values from the `config.yaml` file.
-    They should follow the structure of `config.yaml`, which includes sections like 
-    'launch', 'queue', 'layout', 'model', and 'advanced'.
+        Initializes the Gradio web interface with settings from a YAML configuration file
+        and/or keyword arguments. The function manages AI models, handling their loading 
+        into RAM and unloading after a session or specified timeout.
 
-    Args:
-        config (str): Path to the YAML configuration file. Default settings are used 
-                      if not provided.
-        **kwargs: Keyword arguments corresponding to the configuration sections. Each 
-                  argument should be a dictionary reflecting the structure of its 
-                  respective section in `config.yaml`.
+        Returns:
+            None
+        """
+        print("Starting Gradio Web Interface")
 
-    Returns:
-        None
-    """
-
-    # Load and override configuration from the YAML file with kwargs
-    
-    config = AppConfigLoader.load_config(config, **kwargs)
-    
-    # Set the layout for the Gradio interface
-
-    print("Starting Gradio Web Interface")
-    
-    # Launch the Gradio interface
-    
-    interface = gradio_Interface(config)
-    interface.queue(**config.queue)
-    interface.launch(**config.launch)
-  
-    
-    # # Set the layout for the Gradio interface
-    # layout = config.get_layout()
-    
-    # # start the timer thread
-    # timer.start()
-    
-    # print("Starting Gradio Web Interface")
-    
-    # # Launch the Gradio interface
-    
-    # interface = gradio_Interface(layout)
-    # interface.queue(**config.queue)
-    # interface.launch(**config.launch)
-
-    # # Wait for the timer thread to finish
-    # timer.join()
-    # gv.MODELS_PROCESS.join()
+        interface = gradio_Interface(self.config)
+        interface.queue(**self.config.queue)
+        interface.launch(**self.config.launch)

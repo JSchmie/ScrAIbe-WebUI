1e68cc9cbfde13b7f65af2d9dc48f4414298b666
Schmieder, Jacob <jacob.schmieder@dbfz.de>
2024-05-28 13:20:23 +0000

    make cli easier

--- scraibe_webui/cli.py
+++ scraibe_webui/cli.py
@@ -11,6 +11,7 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 from .utils._parsekwargs import ParseKwargs
 from .global_var import ROOT_PATH
+from .app import app
 
 def cli():
     """
@@ -18,38 +19,21 @@ def cli():
     """
 
     parser = ArgumentParser(formatter_class = ArgumentDefaultsHelpFormatter)
-
-    group = parser.add_mutually_exclusive_group()
     
     parser.add_argument("-c","--config", type=str, default= None,
                         help="Path to the customized config.yaml file.")
     
     parser.add_argument('--server-kwargs', nargs='*', action=ParseKwargs, default={},
                     help='Keyword arguments for the Gradio app. If you do not provide a config file, you can use this to set the server configuration.')
-    
-    group.add_argument('--start-server', action='store_true',
-                        help='Start the Gradio app.' \
-                        'If set, all other arguments are ignored' \
-                        'besides --server-config or --server-kwargs.')
 
     args = parser.parse_args()
     
     arg_dict = vars(args)
     
-    execute_path = os.path.join(ROOT_PATH, "scraibe_webui/app_starter.py")
-    
     config = arg_dict.pop("config")
     
-    server_kwargs = arg_dict.pop("server_kwargs")
-    # TODO : Change this to the new interface type
-    if not config and not server_kwargs:
-        subprocess.run([sys.executable, execute_path])
-    if not config and server_kwargs:
-        subprocess.run([sys.executable, execute_path, f"--server-kwargs={server_kwargs}"])
-    elif not server_kwargs and config:
-        subprocess.run([sys.executable, execute_path, f"--server-config={config}"])
-    else:
-        subprocess.run([sys.executable, execute_path, f"--server-config={config}", f"--server-kwargs={server_kwargs}"])
-
+    app(config, **arg_dict)
+    
+    
 if __name__ == "__main__":
     cli()
